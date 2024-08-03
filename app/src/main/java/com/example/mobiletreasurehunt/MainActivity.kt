341ec4d75f363b1f3ee6e7b0d439450608b89947
Steven Bertolucci <steven.bertolucci@yahoo.com>
2024-08-03 09:29:15 -0700

    First push

---
+++ app/src/main/java/com/example/mobiletreasurehunt/MainActivity.kt
@@ -0,0 +1,36 @@
+// Author: Steven Bertolucci
+// Course: CS 492 - Mobile Application Development
+// Institution: Oregon State University
+
+package com.example.mobiletreasurehunt
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mobiletreasurehunt.ui.theme.MobileTreasureHuntTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MobileTreasureHuntTheme {
+                TreasureHunt()
+            }
+        }
+    }
+}
+
+@Composable
+fun TreasureHunt() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TreasureHuntPreview() {
+    MobileTreasureHuntTheme {
+        TreasureHunt()
+    }
+}
---
+++ app/src/main/java/com/example/mobiletreasurehunt/TreasureHuntScreen.kt
@@ -0,0 +1,6 @@
+// Author: Steven Bertolucci
+// Course: CS 492 - Mobile Application Development
+// Institution: Oregon State University
+
+package com.example.mobiletreasurehunt
+
