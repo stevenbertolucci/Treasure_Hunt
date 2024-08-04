791762d3cf1fe5e4d52734f366cab47e66e99903
stevenbertolucci <steven.bertolucci@yahoo.com>
2024-08-03 17:30:28 -0700

    Added Request Permission Screen

---
+++ app/src/main/java/com/example/mobiletreasurehunt/ui/screens/requestPermissionScreen/RequestPermission.kt
@@ -0,0 +1,18 @@
+// Author: Steven Bertolucci
+// Course: CS 492 - Mobile Application Development
+// Institution: Oregon State University
+
+package com.example.mobiletreasurehunt.ui.screens.requestPermissionScreen
+
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RequestPermissionScreen(
+    onStartButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Text(text = "Under construction")
+}
