a3fd479b9a0cc227c7e4fa5e068b4d0d3c736005
stevenbertolucci <steven.bertolucci@yahoo.com>
2024-08-03 17:28:25 -0700

    Added Rules Screen

---
+++ app/src/main/java/com/example/mobiletreasurehunt/ui/screens/rulesScreen/RulesScreen.kt
@@ -0,0 +1,17 @@
+// Author: Steven Bertolucci
+// Course: CS 492 - Mobile Application Development
+// Institution: Oregon State University
+
+package com.example.mobiletreasurehunt.ui.screens.rulesScreen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RulesScreen(
+    onStartButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Text(text = "Under construction")
+}
